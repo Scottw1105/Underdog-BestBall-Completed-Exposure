537267a0455cdc275ba5205990a6ece007ef468a
Scottw1105 <scottw1105@gmail.com>
2024-04-30 12:50:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# UNDERDOG-COMPLETED-BESTBALL-DRAFTS-EXTRACT
+
+Extract your results from the "Drafts-Completed" page on the Underdog fantasy website for daily contests.
+
+## Installation
+
+1. To begin installation, you must ensure that "Developer mode" is enabled in your chrome extensions. To do this, navigate to `chrome://extensions/` in your browser, and click the toggle icon in the top right. You will also need a .zip file extractor, such as WinRAR or 7zip.
+
+2. Download this repository as a .zip file. At the top of this page, there is a green button that says "Code". Click it, and select "Download ZIP" from the dropdown menu.
+
+3. Open the downloaded .zip file, click into the folder that says `Underdog-Results-Extract-main`, then again into the `zip` folder. Select the .zip file and extract it somewhere on your computer.
+
+4. Navigate back to `chrome://extensions/` in your browser. In your operating system's file explorer, select that .zip file you just extracted, then drag & drop it onto the chrome extension page. You should now see the extension on that page.
+
+5. To use the extension, click the puzzle-piece looking icon in the top right of Google Chrome, and "pin" the extension. Once you see it in the list of extension icons.
+
+## Usage
+
+1. Log into your Underdog account on `www.UnderdogFantasy.com`.
+
+2. Click on the `Drafts` tab along the top of the page.
+
+3. Ensure `Completed` is selected just below the heading.
+
+4. Choose the tournament you'd like to extract the results for.
+
+5. Click the `B` Chrome extension button that is pinned to your extension bar.
+
+6. Save resulting CSV file.
+
+Note: You'll have to do this for each tournament individually. This was done so that if a tournament is max entered, you don't keep getting the exposures from it over and over as this is run.
---
+++ content.js
@@ -0,0 +1,77 @@
+var lineup_info = [["Draft_ID", 'Pick_Position', 'Build', 'Name', 'Position', 'Team', 'ADP' ,'Pick']]
+
+var tourneyName = document.querySelector(".styles__tournamentTitle___PrZU").textContent.trim();
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function exportToCSV(filename, csvData) {
+    const csvBlob = new Blob([csvData], { type: "text/csv" });
+    const csvURL = URL.createObjectURL(csvBlob);
+
+    const a = document.createElement("a");
+    a.href = csvURL;
+    a.download = filename;
+
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+
+    // Cleanup
+    document.body.removeChild(a);
+    URL.revokeObjectURL(csvURL);
+}
+
+async function get_exposures() {
+    // Get all the lineup Div's
+    const entries = Array.from(document.querySelectorAll(".styles__draftCell__pWlSX"));
+
+    for (var i = 0; i < entries.length; i++) {
+        await entries[i].click();
+        await sleep(750);
+        //Gather base info for each lineup that will be the same amongst all players in each lineup. Draft ID, Pick Position, Build. 
+        var lineup_base = [];
+
+        var draft_id = entries[i].getAttribute("data-draft-id");
+        lineup_base.push(draft_id);
+
+        var pick_position = document.querySelector(".styles__pickAndProjectionValue__Ugzds").textContent.trim();
+        lineup_base.push(pick_position);
+
+        var build = Array.from(document.querySelectorAll(".styles__positionCount__J96ya"));
+        let qb = build[0].textContent.trim();
+        let rb = build[1].textContent.trim();
+        let wr = build[2].textContent.trim();
+        let te = build[3].textContent.trim();
+        let build_list = `${qb}-${rb}-${wr}-${te}`;
+        lineup_base.push(build_list);
+
+        // Get all player DIVs
+        const positions = Array.from(document.querySelectorAll(".styles__positionSection__N_raC"));
+        for (position = 0; position < positions.length; position++) {
+
+            const players = Array.from(positions[position].querySelectorAll(".styles__playerPickCell__JCEIA"));
+            var player_pos = positions[position].querySelector(".styles__positionHeader__R2EgT").textContent.trim();
+            var player_list = [];
+            for (var player = 0; player < players.length; player++) {
+                temp_list = [];
+                var Name = players[player].querySelector(".styles__infoValue__abWUk").textContent.trim();
+                var Team = players[player].querySelector(".styles__infoKey__u67Lp").textContent.trim();
+                var add_info = Array.from(players[player].querySelector(".styles__additionalInfo__gNhRa").querySelectorAll(".styles__infoValue__abWUk"));
+                var adp = add_info[0].textContent.trim();
+                var pick = add_info[1].textContent.trim();
+                
+                temp_list = temp_list.concat(lineup_base);
+                temp_list.push(Name, player_pos, Team, adp, pick);
+            
+                lineup_info.push(temp_list);
+            };
+        };
+        await sleep(2500);
+    }
+    console.log(lineup_info);
+    exportToCSV(tourneyName, lineup_info.join("\n"));
+};
+
+get_exposures()
